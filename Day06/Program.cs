390981df19f7ddb39b6a49e7e10fa6e46b6b78ca
Carlos Manrique Enguita <9149238+carlosmanri@users.noreply.github.com>
2023-12-06 15:13:28 +0100

    Day 06 part one, MRU goes brrr

---
+++ Day06/Program.cs
@@ -0,0 +1,24 @@
+﻿using Day06;
+using System.Text.RegularExpressions;
+
+var lines = File.ReadLines("input.txt");
+
+var times = Regex.Matches(lines.First(), "\\d+").Select(m => int.Parse(m.Value));
+var distances = Regex.Matches(lines.ElementAt(1), "\\d+").Select(m => int.Parse(m.Value));
+
+var races = times.Zip(distances, (t, d) => new Race { MaxTime = t, RecordDistance = d });
+
+
+var result = races.Select(r =>
+    {
+        var sqrt = Math.Sqrt(Math.Pow(r.MaxTime, 2) - 4 * r.RecordDistance);
+        var x1 = (r.MaxTime + sqrt) / 2;
+        var x2 = (r.MaxTime - sqrt) / 2;
+
+        return (int)x1 - (int)x2;
+
+    }).Aggregate((x, y) => x * y);
+
+Console.WriteLine("Part one solution: " + result);
+
+
---
+++ Day06/Race.cs
@@ -0,0 +1,8 @@
+﻿namespace Day06;
+
+public class Race
+{
+    public int MaxTime { get; set; }
+    public int RecordDistance { get; set; }
+
+}
