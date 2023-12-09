3f08fddb35a780253fff90079c20cc7ccc7629ae
Carlos Manrique Enguita <9149238+carlosmanri@users.noreply.github.com>
2023-12-09 20:03:07 +0100

    Day 08 part two

--- Day08/Program.cs
+++ Day08/Program.cs
@@ -14,7 +14,6 @@ lines.ForEach(line => {
     graph.Add(match[0].Value, new Network { Left = match[1].Value, Right = match[2].Value });
 });
 
-
 string currentNode = "AAA";
 int steps = 0;
 foreach (var instruction in instructions.IterateInfinitely())
@@ -29,6 +28,52 @@ Console.WriteLine("Part one solution: " + steps);
 
 
 
+lines = File.ReadLines("input.txt").ToList();
+
+instructions = lines.First();
+
+lines.RemoveRange(0, 2);
+
+graph = [];
+
+lines.ForEach(line => {
+    var match = Regex.Matches(line, "[A-Z0-9]{3}");
+    graph.Add(match[0].Value, new Network { Left = match[1].Value, Right = match[2].Value });
+});
+
+var currentNodes = graph.Keys.Where(k => k.EndsWith('A')).ToList();
+
+var allSteps = currentNodes.Select(node => {
+    long step = 0;
+    while (true)
+    {
+        if (node.EndsWith('Z')) break;
+
+        node = instructions[(int)step % instructions.Length].Equals('L') ? graph[node].Left : graph[node].Right;
+        step++;
+    }
+    return step;
+});
+
+Console.WriteLine("Part two solution: " + LCM(allSteps.ToArray()));
+
+
+
+
+static long LCM(long[] numbers)
+{
+    return numbers.Aggregate(lcm);
+}
+static long lcm(long a, long b)
+{
+    return Math.Abs(a * b) / GCD(a, b);
+}
+static long GCD(long a, long b)
+{
+    return b == 0 ? a : GCD(b, a % b);
+}
+
+
 public static class IEnumerableHelper
 {
     public static IEnumerable<T> IterateInfinitely<T>(this IEnumerable<T> sequence)
@@ -38,3 +83,4 @@ public static class IEnumerableHelper
                 yield return item;
     }
 }
+
